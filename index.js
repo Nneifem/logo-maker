a04549fa7fd80a6b58e861fca1c7a91d8ce73fb0
Nneif <nneifemakpunonu@gmail.com>
2023-05-24 14:06:23 -0400

    have the logo.svg to generate but showing undefined on the input places

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 /* will format how the the shape and color will look */
+function renderShape(shape){
+    if(shape === 'triangle'){
+        triangle = 'polygon points="150, 18 244, 182 56, 182"';
+        return shape;
+    }
+    else if(shape === 'circle'){
+        circle = 'circle cx="25" cy="75" r="20" height="30"';
+        return shape;
+    }
+    else if(shape === 'square'){
+        square = 'squ x="10" y="10" width="30" height="30"';
+        return shape;
+    }
+}
+
 const generateLogo = (text, textColor, shape, backgroundColor) =>
 `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-<${shape} cx="150" cy="100" r="80" fill="${backgroundColor}" />
+<${shape} ${renderShape(shape)} fill="${backgroundColor}" />
 
 <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
 
@@ -16,7 +31,8 @@ const questions = [
     {
         type: 'input',
         message: 'Enter a three character text',
-        name: 'text'
+        name: 'text',
+        maxLength: 3
 
     },
     {
@@ -50,3 +66,18 @@ const questions = [
     }
 ];
 
+function writeToFile(fileName, data){
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.log(err) : console.log('Generated logo.svg')
+    );
+}
+
+function init(){
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const createLogo = generateLogo(answers)
+        writeToFile('logo.svg', createLogo);
+    })
+}
+init();
